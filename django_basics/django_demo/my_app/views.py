5027f1b99dcf25591fa751ff7cb20bfdb135a089
ds7cw <122825406+ds7cw@users.noreply.github.com>
2024-01-19 16:01:42 +0000

    Signin & signout functionality updated

--- django_basics/django_demo/my_app/views.py
+++ django_basics/django_demo/my_app/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
 
 
 # Create your views here.
@@ -19,22 +20,57 @@ def signup(request):
         pass1 = request.POST['pass1']
         pass2 = request.POST['pass2']
 
-        if pass1 == pass2:
-            my_user = User.objects.create_user(username=username, email=email_addres, password=pass1)
-            my_user.last_name = lname
-            my_user.first_name = fname
-            my_user.save()
+        if User.objects.filter(username=username):
+            messages.error(request, 'This username already exists!')
+            return redirect('home')
+        
+        if len(username) < 3:
+            messages.error(request, 'Username must be at least 3 characters long!')
+            return redirect('home')
+        
+        if not username.isalnum():
+            messages.error(request, 'Username must contain only Alpha-Numeric characters!')
+            return redirect('home')
 
-            messages.success(request, 'Account has been successfully created!')
-            return redirect('signin')
+        if User.objects.filter(email=email_addres):
+            messages.error(request, 'This email is already registered!')
+            return redirect('home')
+        
+        if pass1 != pass2:
+            messages.error(request, 'The password and confirm password do not match!')
+            return redirect('home')
+
+        my_user = User.objects.create_user(username=username, email=email_addres, password=pass1)
+        my_user.last_name = lname
+        my_user.first_name = fname
+        my_user.save()
+
+        messages.success(request, 'Account has been successfully created!')
+        return redirect('signin')
     else:
         return render(request, 'authentication/signup.html')
 
 
 def signin(request):
+
+    if request.method == 'POST':
+        username = request.POST['username']
+        pass1 = request.POST['pass1']
+
+        user = authenticate(username=username, password=pass1)
+
+        if user is not None:
+            login(request, user)
+            return render(request, 'main/home.html')
+        
+        else:
+            messages.error(request, 'Invalid Credentials!')
+            return redirect('signin')
+
     return render(request, 'authentication/signin.html')
 
 
 def signout(request):
-
+    logout(request)
+    messages.success(request, 'Logged out successfully!')
     return redirect('home')
